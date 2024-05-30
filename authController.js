39b5378b1f0fd9f06296d94f32514f1d97d23d56
Temirlan Tassaliyev <160610034+Temoojeen@users.noreply.github.com>
2024-05-30 18:40:35 +0500

    add middleware

--- authController.js
+++ authController.js
@@ -56,7 +56,8 @@ class authController {
     }
     async getUsers(req, res) {
         try {
-            res.json("server works")
+            const users = await User.find()
+            res.json(users)
         } catch (e) {
             
         }
--- authRouter.js
+++ authRouter.js
@@ -2,10 +2,13 @@ const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
 const {check} = require('express-validator')
+const authMiddleware = require('./middleware/authMiddleWare')
+const roleMiddleware = require('./middleware/roleMiddleWare')
 
 router.post('/registration',check('username',"Имя пользователя не может быть пустым").notEmpty(),
 check('password',"Пароль должен быть больше 4 и меньше 10 символов").isLength({min: 4, max:10}),
  controller.registration)
 router.post('/login', controller.login)
-router.get('/users', controller.getUsers)
+router.get('/users',roleMiddleware(["ADMIN","USER"]), controller.getUsers)
+// router.get('/users',authMiddleware, controller.getUsers)
 module.exports = router
